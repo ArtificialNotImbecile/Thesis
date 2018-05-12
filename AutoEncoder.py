bd7cdbf52cbf0a8ec1c8cf174cf87ea5af78a801
ani <rowyoung@icloud.com>
2018-05-12 15:24:02 +0800

    First commit

---
+++ AutoEncoder.py
@@ -0,0 +1,110 @@
+import numpy as np
+from keras.layers import Input, Dense
+from keras.models import Model
+from keras import regularizers
+import matplotlib.pyplot as plt
+
+class AutoEncoder(object):
+    def __init__(self, layers=[784, 32, 784], activation='relu',regularize=False):
+        self.num_layers = len(layers)
+        self.layers = layers
+        assert self.num_layers%2!=0
+        self.activation = activation
+        self.regularize = regularize
+        
+    def compile(self, optimizer='adadelta', loss='binary_crossentropy'):
+        if self.regularize:
+            #encoder part
+            input_img = Input(shape=(self.layers[0],))
+            encoded = Dense(self.layers[1], activation=self.activation, activity_regularizer=regularizers.l1(10e-5))(input_img)
+            for i in range(int((self.num_layers-3)/2)):
+                encoded = Dense(self.layers[i+2], activation=self.activation, activity_regularizer=regularizers.l1(10e-5))(encoded)
+            # decoder part
+            if self.num_layers != 3:
+                decoded = Dense(self.layers[int((self.num_layers+1)/2)], activation=self.activation, activity_regularizer=regularizers.l1(10e-5))(encoded)
+            for i in range(int((self.num_layers-3)/2-1)):
+                j = i + int((self.num_layers+3)/2)
+                decoded = Dense(self.layers[j], activation=self.activation, activity_regularizer=regularizers.l1(10e-5))(decoded)
+            # ???????????????is this correct????????????????sigmoid? and don't use regularizers?
+            if self.num_layers !=3:
+                decoded = Dense(self.layers[-1], activation='sigmoid')(decoded)
+            else:
+                decoded = Dense(self.layers[-1], activation='sigmoid')(encoded)
+            self.autoencoder = Model(input_img, decoded)
+            self.encoder = Model(input_img, encoded)
+            # Decoder is a little bit tricker
+            encoded_input = Input(shape=(self.layers[int((self.num_layers-1)/2)],))
+            # retrieve the last layer of the autoencoder model
+            decoder_layer = self.autoencoder.layers[int((self.num_layers+1)/2)](encoded_input)
+            for i in range(int((self.num_layers-3)/2)):
+                j = i + int((self.num_layers+3)/2)
+                decoder_layer = self.autoencoder.layers[j](decoder_layer)
+            # create the decoder model
+            self.decoder = Model(encoded_input, decoder_layer)
+            self.autoencoder.compile(optimizer=optimizer, loss=loss)
+        else:
+            #encoder part
+            input_img = Input(shape=(self.layers[0],))
+            encoded = Dense(self.layers[1], activation=self.activation)(input_img)
+            for i in range(int((self.num_layers-3)/2)):
+                encoded = Dense(self.layers[i+2], activation=self.activation)(encoded)
+            # decoder part
+            if self.num_layers!=3:
+                decoded = Dense(self.layers[int((self.num_layers+1)/2)], activation=self.activation)(encoded)
+            for i in range(int((self.num_layers-3)/2-1)):
+                j = i + int((self.num_layers+3)/2)
+                decoded = Dense(self.layers[j], activation=self.activation)(decoded)
+            # ???????????????is this correct????????????????
+            if self.num_layers !=3:
+                decoded = Dense(self.layers[-1], activation='sigmoid')(decoded)
+            else:
+                decoded = Dense(self.layers[-1], activation='sigmoid')(encoded)
+            self.autoencoder = Model(input_img, decoded)
+            self.encoder = Model(input_img, encoded)
+            # Decoder is a little bit tricker
+            encoded_input = Input(shape=(self.layers[int((self.num_layers-1)/2)],))
+            # retrieve the last layer of the autoencoder model
+            decoder_layer = self.autoencoder.layers[int((self.num_layers+1)/2)](encoded_input)
+            for i in range(int((self.num_layers-3)/2)):
+                j = i + int((self.num_layers+3)/2)
+                decoder_layer = self.autoencoder.layers[j](decoder_layer)
+            # create the decoder model
+            self.decoder = Model(encoded_input, decoder_layer)
+            self.autoencoder.compile(optimizer=optimizer, loss=loss)
+            
+    def fit(self, x, y, epochs=100, batch_size=256, shuffle=True, **kwarg):
+        self.autoencoder.fit(x, y,
+                epochs=epochs,
+                batch_size=batch_size,
+                shuffle=shuffle,
+                **kwarg)
+        
+    def get_encoded_imgs(self, x_new):
+        return self.encoder.predict(x_new)
+    
+    def get_decoded_imgs(self, x_new):
+        encoded_imgs = self.encoder.predict(x_new)
+        return self.decoder.predict(encoded_imgs)
+    
+    def comparison_plot(self, x_new, n=10, figsize=(20,4)):
+        plt.figure(figsize=(20, 4))
+        decoded_imgs = self.get_decoded_imgs(x_new)
+        for i in range(n):
+            # display original
+            ax = plt.subplot(2, n, i + 1)
+            plt.imshow(1-x_new[i].reshape(28, 28))
+            plt.gray()
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+
+            # display reconstruction
+            ax = plt.subplot(2, n, i + 1 + n)
+            plt.imshow(1-decoded_imgs[i].reshape(28, 28))
+            plt.gray()
+            ax.get_xaxis().set_visible(False)
+            ax.get_yaxis().set_visible(False)
+        plt.show()
+        
+def add_noise_to_data(x, noise_factor=0.5):
+        x_noisy = x + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x.shape)
+        return np.clip(x_noisy, 0., 1.)    
---
+++ SimulationData.py
@@ -0,0 +1,84 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import cluster, datasets, mixture
+from sklearn.neighbors import kneighbors_graph
+from sklearn.preprocessing import StandardScaler
+from itertools import cycle, islice
+np.random.seed(0)
+
+class SimulationData(object):
+    """
+        Datasets: leftdown, center, double_center, half_moon, two_spirals
+        Method: plot
+    """
+    np.random.seed(0)
+    def __init__(self, total_num, minor_num, rs=8):
+        self.total_num = total_num
+        self.minor_num = minor_num
+        self.rs = rs
+
+    def plot(self, data):
+        colors = np.array(list(islice(cycle(['#377eb8', '#ff7f00', '#4daf4a',
+                                             '#f781bf', '#a65628', '#984ea3',
+                                             '#999999', '#e41a1c', '#dede00']),
+                              int(max(data[1]) + 1))))
+        plt.scatter(data[0][:, 0], data[0][:, 1], s=10, color=colors[1-data[1]])
+        plt.show()
+
+    @property
+    def leftdown(self):
+        blobs = np.random.uniform([0,0],[2,2],
+                                  size=[self.minor_num,2]), np.repeat(0,self.minor_num)
+        no_structure = np.random.rand(self.total_num, 2)*12, None
+        data1 = [[i,j] for i,j in zip(no_structure[0][:,0],no_structure[0][:,1]) if not ((0<=i<=2) and (0<=j<=2))]
+        data1 = np.array(data1)
+        data1 = data1,np.repeat(1,data1.shape[0])
+        data = np.concatenate((data1[0],blobs[0])),np.concatenate((data1[1],blobs[1]))
+        return data
+
+    @property
+    def center(self):
+        blobs = np.random.uniform([5,5],[7,7],
+                                  size=[self.minor_num,2]), np.repeat(0,self.minor_num)
+        no_structure = np.random.rand(self.total_num, 2)*12, None
+        data1 = [[i,j] for i,j in zip(no_structure[0][:,0],no_structure[0][:,1]) if not ((5<=i<=7) and (5<=j<=7))]
+        data1 = np.array(data1)
+        data1 = data1,np.repeat(1,data1.shape[0])
+        data = np.concatenate((data1[0],blobs[0])),np.concatenate((data1[1],blobs[1]))
+        return data
+
+    @property
+    def double_center(self):
+        blobs = datasets.make_blobs(n_samples=self.minor_num, random_state=self.rs, centers= [[10,10], [7,7]],cluster_std=0.4)
+        no_structure = np.random.rand(self.total_num, 2)*9+3, None
+        data1 = [[i,j] for i,j in zip(no_structure[0][:,0],no_structure[0][:,1]) if not ((6<=i<=8) and (6<=j<=8))]
+        data1 = np.array(data1)
+        data1 = [[i,j] for i,j in zip(data1[:,0],data1[:,1]) if not ((9<=i<=11) and (9<=j<=11))]
+        data1 = np.array(data1)
+        data1 = data1,np.repeat(1,data1.shape[0])
+        labels = np.repeat(0,len(blobs[1]))
+        data = np.concatenate((data1[0],blobs[0])),np.concatenate((data1[1],labels))
+        return data
+
+    @property
+    def half_moon(self):
+        noisy_moons = datasets.make_moons(n_samples=self.total_num, noise=.02,random_state=self.rs)
+        noisy_moons1 = datasets.make_moons(n_samples=self.minor_num, noise=.03, random_state=8)
+        data_moon = noisy_moons[0][noisy_moons[1]==1,:],np.repeat(1,int(self.total_num/2))
+        data_moon1 = 0.8-noisy_moons1[0][noisy_moons1[1]==0,:]+[0.23,0],np.repeat(0,int(self.minor_num/2))
+        data = np.concatenate((data_moon[0],data_moon1[0])),np.concatenate((data_moon[1],data_moon1[1]))
+        return data
+
+    @property
+    def two_spirals(self):
+        n = np.sqrt(np.random.rand(self.total_num,1)) * 780 * (2*np.pi)/360
+        d1x = -np.cos(n)*n + np.random.rand(self.total_num,1) * 0.5
+        d1y = np.sin(n)*n + np.random.rand(int(self.total_num),1) * 0.5
+        X,y = (np.vstack((np.hstack((d1x,d1y)),np.hstack((-d1x,-d1y)))),
+                np.hstack((np.zeros(self.total_num),np.ones(self.total_num))))
+        X1 = np.array((X[y==0,0][:self.minor_num], X[y==0,1][:self.minor_num])).transpose()  #######use Transpose instead of reshape!!!!!
+        X2 = np.array((X[y==1,0], X[y==1,1])).transpose()
+        X_12 = np.concatenate((X1,X2))
+        y_12 = np.concatenate((np.repeat(0,self.minor_num),np.repeat(1,1000)))
+        data = X_12,y_12
+        return data
